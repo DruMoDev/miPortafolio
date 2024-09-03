2015f6cbcb5b246035709217e12498144be44018
DruMoDev <dev.morera@gmail.com>
2024-09-03 11:28:00 +0200

    Width and styles improveds

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -4,11 +4,11 @@ import MisProyectos from "../components/proyectos/MisProyectos";
 
 const LandingPage = () => {
   return (
-    <>
+    <main className="max-w-[1024px]">
       <AboutMe />
       <MisProyectos />
       <Contacto />
-    </>
+    </main>
   );
 };
 export default LandingPage;
